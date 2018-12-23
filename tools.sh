e8a8a75e8521920bf987cc15190477a4202e9209
Braden Mars <BradenM@users.noreply.github.com>
2018-12-23 17:22:44 -0600

    feat: Added Some Scripts and Geekbench Score

---
+++ tools.sh
@@ -0,0 +1,8 @@
+# Rebuild Kernal Cache
+alias kernal_rebuild="sudo touch /System/Library/Extensions && sudo kextcache -i /"
+
+# Open System Kexts folder
+alias kexts="open /System/Library/Extensions"
+
+# Open L/E Kexts Folder
+alias lekexts="open /Library/Extensions"
