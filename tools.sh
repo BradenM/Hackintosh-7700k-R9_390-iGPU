88b396947ef6b0a37331cf50c62bce9e4a6b9716
Braden Mars <BradenM@users.noreply.github.com>
2018-12-23 20:00:31 -0600

    feat(Tools): Add zsh shebang, Add repair_kexts script

--- tools.sh
+++ tools.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 # Rebuild Kernal Cache
 alias kernal_rebuild="sudo touch /System/Library/Extensions && sudo kextcache -i /"
 
@@ -6,3 +8,14 @@ alias kexts="open /System/Library/Extensions"
 
 # Open L/E Kexts Folder
 alias lekexts="open /Library/Extensions"
+
+# Repair Permissions
+repair_kexts () {
+	echo "Repairing Kexts..."
+	sudo chown -R root:wheel /Library/Extensions/*.kext
+	sudo chmod -R 755 /Library/Extensions/*.kext
+	echo "Finished. Rebuilding Cache..."
+	sleep 4
+	kernal_rebuild
+	echo "Kext Repair Complete"
+}
